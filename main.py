350dbf7b90eea403ed027b10217315e1fafe76ff
Mayank Deopa <mayankdeopa999@gmail.com>
2023-02-16 11:02:47 +0530

    utils file config file updated

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 from insurance.logger import logging
 from insurance.exception import InsuranceException
-
+from insurance.utils import get_collection_as_dataframe
 import sys, os
- 
-def test_logger_and_expetion():
+# from Insurance.entity.config_entity import DataIngestionConfig
+# from Insurance.entity import config_entity
+# from Insurance.components.data_ingestion import DataIngestion
+# from Insurance.components.data_validation import DataValidation
 
+# from Insurance.components.data_transformation import DataTransformation
 
-
-   try:
-
-
-      logging.info("Starting the test_logger_and_exception")
-      result = 3/0
-      print(result)
-      logging.info("Stoping the test_logger_and_exception")
-   except Exception as e:
-
-      logging.debug(str(e))
-      raise InsuranceException(e, sys)
+#def test_logger_and_expection():
+   # try:
+       # logging.info("Starting the test_logger_and_exception")
+        #result = 3/0
+       # print(result)
+       # logging.info("Stoping the test_logger_and_exception")
+    #except Exception as e:
+      #  logging.debug(str(e))
+       # raise InsuranceException(e, sys)
 
 if __name__=="__main__":
-   try:
-      test_logger_and_expetion()
-   except Exception as e:
-      print(e)   
+     try:
+          #start_training_pipeline()
+          #test_logger_and_expection()
+         get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
+      #  training_pipeline_config = config_entity.TrainingPipelineConfig()
+       
+      #data ingestion
+      #  data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+      #  print(data_ingestion_config.to_dict())
+      #  data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+      #  data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+       
+       # Data Validation
+      #  data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+      #  data_validation = DataValidation(data_validation_config=data_validation_config,
+      #                    data_ingestion_artifact=data_ingestion_artifact)
+        
+      #  data_validation_artifact = data_validation.initiate_data_validation()
+
+      #Data Transformation
+
+       
+      
+     except Exception as e:
+          print(e)
